424797ad15667b75d12f69e2a6ef21214a4e9ed2
Gilberto Martini <con-valniria.bandeira@telemetria-supergasbras.com.br>
2021-02-16 17:32:54 -0300

    chore(layouts): Working with layouts

---
+++ App04_Xamarin/App04_Xamarin/App04_Xamarin/Master/Menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App04_Xamarin.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        public void GoPaginaPerfil(object sender, EventArgs e)
+        {
+            Detail = new Pages.Perfil();
+            IsPresented = false;
+        }
+
+        public void GoPaginaXamarin(object sender, EventArgs e)
+        {
+            Detail = new Pages.Xamarin();
+            IsPresented = false;
+        }
+
+    }
+}
