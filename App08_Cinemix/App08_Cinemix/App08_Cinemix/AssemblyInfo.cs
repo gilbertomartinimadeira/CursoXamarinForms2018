8a29bcaf8540941741a1c6cf4262ffc4ae098384
Gilberto Martini <con-valniria.bandeira@telemetria-supergasbras.com.br>
2021-03-10 18:36:01 -0300

    chore(styling): Added Styles to cinemix project

---
+++ App08_Cinemix/App08_Cinemix/App08_Cinemix/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Font Awesome 5 Free-Regular-400.otf", Alias = "FontAwesomeRegular")]
+[assembly: ExportFont("segoeui.otf", Alias = "segoeui")]
+
